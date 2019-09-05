1fa8e267709848dd972f3cc38c3b1c91213e3af1
Gerben van Adrichem <gerben.van.adrichem@valueblue.nl>
2019-09-05 10:28:42 +0200

    Initial version

---
+++ specflowselenium/Bindings/SpecflowBindings.cs
@@ -0,0 +1,25 @@
+﻿namespace specflowselenium.Bindings
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Firefox;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    class SpecflowBindings
+    {
+        private IWebDriver Driver;
+
+        [When(@"I start the browser")]
+        public void WhenIStartTheBrowser()
+        {
+            Driver = new FirefoxDriver();
+        }
+
+        [When(@"I navigate to '(.*)'")]
+        public void WhenINavigateToHttpExample_Com(string Url)
+        {
+            Driver.Navigate().GoToUrl(Url);
+        }
+
+    }
+}
